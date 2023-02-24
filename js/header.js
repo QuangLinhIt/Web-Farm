c2cc80ff964cdbfb9e47b4775da47ab5d6703695
2k1QuangLinh <118100679+2k1QuangLinh@users.noreply.github.com>
2023-02-24 08:19:39 +0700

    upload lan1

---
+++ js/header.js
@@ -0,0 +1,12 @@
+document.querySelector('#menu').innerHTML = ` <ul class="menu container-fluid" id="menu1">
+<li><a class="nav-link" href="./index.html">Homepage</a></li>
+<li><a class="nav-link"  href="./service.html">Service</a></li>
+<li><a class="nav-link"  href="./product.html">Product</a></li>
+<li><a class="nav-link"  href="./introduce.html">Introduce</a></li>
+</ul>
+
+<div class="bar">
+<input type="text" name="" id="search"><i class="fa fa-search"></i>
+<i id="open" onclick="openMenu()" class="fa fa-bars"></i>
+<i id="close" onclick="closeMenu()" class="fa fa-bars"></i>
+</div>`
